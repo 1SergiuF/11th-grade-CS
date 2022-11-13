f61685019de9f85052f5927cf3652f3b9620dd66
Sergiu Fulop <sergiufulop45@gmail.com>
2022-11-13 13:42:37 +0200

    Plata2 - 100p

---
+++ backtracking/pbinfo/Elemente de combinatorica/Plata2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+int n, S;
+int x[20], v[20], c[20];
+
+void CitesteDate();
+void Back(int k, int s);
+bool Ok(int k);
+void ScrieSol(int k);
+
+int main()
+{
+    CitesteDate();
+    Back(1, 0);
+    return 0;
+}
+
+void CitesteDate()
+{
+    cin >> n >> S;
+    for (int i = 1; i <= n; ++i)
+        cin >> v[i];
+
+    for (int i = 1; i <= n; ++i)
+        cin >> c[i];
+}
+
+void Back(int k, int s)
+{
+    if (k > n)
+    {
+        if (s == S)
+        {
+            ScrieSol(k);
+            exit(0);
+        }
+        
+        return;
+    }
+
+    for (int i = 0; s + i * v[k] <= S; ++i)
+    {
+        x[k] = i;
+        if (Ok(k))
+            Back(k + 1, s + i * v[k]);
+    }
+}
+
+bool Ok(int k)
+{
+    if (x[k] == 0 || x[k] > c[k]) return false;
+
+    return true;
+}
+
+void ScrieSol(int k)
+{
+    for (int i = 1; i < k; ++i)
+        cout << x[i] << ' ';
+    cout << '\n';
+}
