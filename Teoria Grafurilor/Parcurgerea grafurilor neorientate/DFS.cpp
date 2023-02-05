7c3da7b7372bc5d2ab81cacbe26a9c9509e8b93d
Sergiu Fulop <sergiufulop45@gmail.com>
2023-02-05 19:38:08 +0200

    DFS - 100p

---
+++ Teoria Grafurilor/Parcurgerea grafurilor neorientate/DFS.cpp
@@ -0,0 +1,39 @@
+#include <fstream>
+using namespace std;
+
+ifstream fin("dfs.in");
+ofstream fout("dfs.out");
+
+#define Dim 101
+bool a[Dim][Dim];
+bool v[Dim];
+int n, m, X;
+
+void CitesteDate();
+void Dfs(int x);
+
+int main()
+{
+    CitesteDate();
+    Dfs(X);
+    return 0;
+}
+
+void CitesteDate()
+{
+    fin >> n >> m >> X;
+    for (int i = 1, x, y; i <= m; ++i)
+    {
+        fin >> x >> y;
+        a[x][y] = a[y][x] = true;
+    }
+}
+
+void Dfs(int x)
+{
+    v[x] = true;
+    fout << x << ' ';
+    for (int y = 1; y <= n; ++y)
+        if (a[x][y] && !v[y])
+            Dfs(y);
+}
