6f8a4de9380557c639da94a28062da0349347107
Sergiu Fulop <sergiufulop45@gmail.com>
2023-02-05 19:30:32 +0200

    Graf Partial 1 - 100p

---
+++ Teoria Grafurilor/Probleme elementare cu grafuri neorientate/Graf_Partial_1.cpp
@@ -0,0 +1,70 @@
+#include <fstream>
+using namespace std;
+
+ifstream fin("graf_partial_1.in");
+ofstream fout("graf_partial_1.out");
+
+#define Dim 101
+
+bool a[Dim][Dim];
+int g[Dim];
+int n;
+int vmin, vmax;
+int cnt;
+
+void CitesteGraf();
+void DeterminaGradMaxSiMin();
+
+int main()
+{
+	CitesteGraf();
+	DeterminaGradMaxSiMin();
+
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			if (a[i][j] && ((g[i] == vmin && g[j] == vmax) || (g[i] == vmax && g[j] == vmin)))
+			{
+				++cnt;
+				a[i][j] = a[j][i] = false;
+			}
+
+	fout << cnt << '\n';
+
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= n; ++j)
+			fout << a[i][j] << ' ';
+		fout << '\n';
+	}
+
+	return 0;
+}
+
+void CitesteGraf()
+{
+	fin >> n;
+	int x, y;
+	while (fin >> x >> y)
+	{
+		if (!a[x][y])
+		{
+			a[x][y] = a[y][x] = true;
+			g[x]++;
+			g[y]++;
+		}
+	}
+}
+
+void DeterminaGradMaxSiMin()
+{
+	vmin = Dim;
+	vmax = -Dim;
+
+	for (int i = 1; i <= n; ++i)
+	{
+		if (vmin > g[i])
+			vmin = g[i];
+		if (vmax < g[i])
+			vmax = g[i];
+	}
+}
