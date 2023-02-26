0b6b6c7d3b9a001df262b5c7bbd1bc06d50d306b
Sergiu Fulop <sergiufulop45@gmail.com>
2023-02-26 18:42:38 +0200

    GradIntNul - 100p

---
+++ Teoria Grafurilor/Probleme diverse cu grafuri orientate/GradIntNul.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+using namespace std;
+
+const int N = 101;
+bool a[N][N];
+int n, m;
+int gi[N];
+int ge[N];
+vector<int> noduri;
+
+void CitesteGraf();
+
+int main()
+{
+    CitesteGraf();
+
+    for (int i = 1; i <= n; ++i)
+        if (!gi[i])
+            noduri.push_back(i);
+
+    
+    std::function<void()> f_display = [&]() {
+        for (auto const& nod : noduri)
+            cout << nod << ' ';
+    };
+
+    if (!noduri.size()) cout << "NU EXISTA";
+    else f_display();
+    
+    return 0;
+}
+
+void CitesteGraf()
+{
+    cin >> n >> m;
+    for (int i = 1, x, y; i <= m; ++i)
+    {
+        cin >> x >> y;
+        a[x][y] = true;
+        ge[x]++;
+        gi[y]++;
+    }
+}
