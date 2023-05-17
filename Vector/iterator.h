d9bb3642bade7f0642d5ff3c3cc5ee3c294cb168
Za Breado <sergiufulop45@gmail.com>
2023-05-17 20:44:18 +0300

    iterator header file

---
+++ Vector/iterator.h
@@ -0,0 +1,70 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+template<
+	template<typename> typename Container,
+	typename T
+>
+class Iterator {
+private:
+	Container<T>& container;
+	int index;	// index (pozitia in container)
+
+public:
+	Iterator(Container<T>& container, int index = 0) : container {container}, index {index}
+	{}
+
+	// ca sa lucreze ca un smart-pointer
+    // trebuie implementate operatiile *, ->, ++
+
+	// operator de dereferentiere
+	T operator *() const
+	{
+		return container[index];
+	}
+
+	T& operator *()
+	{
+		return container[index];
+	}
+
+	// operator de indirectare
+	T* operator -> () const
+	{
+		return container + index;
+	}
+
+	T*& operator -> ()
+	{
+		return container + index;
+	}
+
+	// prefix (infix) increment operator ++it
+	Iterator& operator ++()
+	{
+		index++;
+		return *this;
+	}
+
+	// suffix (postfix) increment operator it++
+	Iterator operator ++(int)
+	{
+		Iterator temp{ *this };
+		index++;
+		return temp;	
+	}
+
+	// operatori relationali
+	
+	bool operator == (Iterator const& it) const noexcept
+	{
+		return index == it.index;
+	}
+
+	bool operator != (Iterator const& it) const noexcept
+	{
+		return !(*this == it);
+	}
+};
+
+#endif // !ITERATOR_H
